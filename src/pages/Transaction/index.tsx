a1563d601b4239ad9a31bcc9668a9e3b188dc65b
Mayro <deehmyller11@gmail.com>
2022-09-02 23:40:50 -0300

    tarefa: adiciona table com lista de entrada e saídas

--- src/pages/Transaction/index.tsx
+++ src/pages/Transaction/index.tsx
@@ -1,13 +1,39 @@
 import React from 'react'
 
 import { Header } from '../../components/Header'
-import { Summary } from '../../components/Summary/index'
+import { Summary } from '../../components/Summary'
+
+import * as S from './style'
 
 export function Transaction() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <S.Container>
+        <S.Table>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <S.Highlight variant="income">R$ 12.000,00</S.Highlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hambúrguer</td>
+              <td>
+                <S.Highlight variant="outcome">R$ 12.000,00</S.Highlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </S.Table>
+      </S.Container>
     </div>
   )
 }
