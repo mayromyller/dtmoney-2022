4d68f62e68a1dc7199e22014ccd84809fe08d6db
Mayro <deehmyller11@gmail.com>
2022-09-02 23:29:47 -0300

    tarefa: atualiza pagina de Transaction com o componente de Summary

--- src/pages/Transaction/index.tsx
+++ src/pages/Transaction/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary/index'
 
 export function Transaction() {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   )
 }
