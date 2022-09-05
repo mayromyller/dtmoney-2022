b152acc0a4ec9ac39a674682c1ff8e0999365a14
Mayro <deehmyller11@gmail.com>
2022-09-05 16:44:34 -0300

    tarefa: adiciona o transactions do context e calcula os valores de entrada, saida e total com reduce

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,8 +1,32 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 
+import { useTransaction } from '../../hooks/useTransaction'
+import { priceFormatter } from '../../utils/formatter'
+
 import * as S from './style'
 
 export function Summary() {
+  const { transactions } = useTransaction()
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      } else {
+        acc.outcome += transaction.price
+        acc.total -= transaction.price
+      }
+
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0
+    }
+  )
+
   return (
     <S.Container>
       <S.Card>
@@ -11,7 +35,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </S.Card>
 
       <S.Card>
@@ -20,16 +44,16 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </S.Card>
 
       <S.Card variant="green">
         <header>
           <span>Total</span>
-          <ArrowCircleDown size={32} color="#fff" />
+          <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </S.Card>
     </S.Container>
   )
