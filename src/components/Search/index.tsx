b2e18a086358af29dd0501ae13c8ea565cb02e37
Mayro <deehmyller11@gmail.com>
2022-09-05 23:56:59 -0300

    tarefa: adiciona tipagem com zod, hook form e função de busca com api

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,10 +1,36 @@
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import { MagnifyingGlass } from 'phosphor-react'
+
+import { useTransaction } from '../../hooks/useTransaction'
+
 import * as S from './style'
 
+const searchSchema = z.object({
+  query: z.string()
+})
+
+type SearchFormInput = z.infer<typeof searchSchema>
+
 export function Search() {
+  const { fetchTransaction } = useTransaction()
+  const { register, handleSubmit } = useForm<SearchFormInput>({
+    resolver: zodResolver(searchSchema)
+  })
+
+  function handleSearchTransaction(data: SearchFormInput) {
+    fetchTransaction(data.query)
+  }
+
   return (
-    <S.Container>
-      <input type="text" placeholder="Busque por transações" />
+    <S.Container onSubmit={handleSubmit(handleSearchTransaction)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
 
       <button type="submit">
         <MagnifyingGlass size={20} />
