857cb20169243e92d327d39dc71f9afe44d6954a
Mayro <deehmyller11@gmail.com>
2022-09-05 23:57:45 -0300

    tarefa: adiciona tipagem com zod, hook form e função para criação de nova transaction

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,9 +1,46 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { X, ArrowCircleDown, ArrowCircleUp } from 'phosphor-react'
 
+import { useForm, Controller } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+
+import { api } from '../../lib/api'
+
 import * as S from './style'
 
+const newTransactionSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  type: z.enum(['income', 'outcome'])
+})
+
+type NewTransactionInputs = z.infer<typeof newTransactionSchema>
+
 export function Modal() {
+  const { register, handleSubmit, control, reset } =
+    useForm<NewTransactionInputs>({
+      resolver: zodResolver(newTransactionSchema),
+      defaultValues: {
+        type: 'income'
+      }
+    })
+
+  async function handleCreteNewTransaction(data: NewTransactionInputs) {
+    const { category, description, price, type } = data
+
+    await api.post('/transactions', {
+      category,
+      description,
+      price,
+      type,
+      createdAt: new Date()
+    })
+
+    reset()
+  }
+
   return (
     <Dialog.Portal>
       <S.Overlay />
@@ -14,22 +51,42 @@ export function Modal() {
           <X size={24} />
         </S.Button>
 
-        <form>
-          <input type="text" placeholder="Descrição" required />
-          <input type="number" placeholder="Preço" required />
-          <input type="text" placeholder="Categoria" required />
+        <form onSubmit={handleSubmit(handleCreteNewTransaction)}>
+          <input
+            type="text"
+            placeholder="Descrição"
+            required
+            {...register('description')}
+          />
+          <input
+            type="number"
+            placeholder="Preço"
+            required
+            {...register('price', { valueAsNumber: true })}
+          />
 
-          <S.Type>
-            <S.TypeButton variant="income" value="income">
-              <ArrowCircleUp size={24} />
-              Entrada
-            </S.TypeButton>
-            <S.TypeButton variant="outcome" value="outcome">
-              <ArrowCircleDown size={24} />
-              Saída
-            </S.TypeButton>
-          </S.Type>
-
+          <input
+            type="text"
+            placeholder="Categoria"
+            required
+            {...register('category')}
+          />
+          <Controller
+            control={control}
+            name="type"
+            render={({ field }) => (
+              <S.Type onValueChange={field.onChange} value={field.value}>
+                <S.TypeButton variant="income" value="income">
+                  <ArrowCircleUp size={24} />
+                  Entrada
+                </S.TypeButton>
+                <S.TypeButton variant="outcome" value="outcome">
+                  <ArrowCircleDown size={24} />
+                  Saída
+                </S.TypeButton>
+              </S.Type>
+            )}
+          />
           <button type="submit">Cadastrar</button>
         </form>
       </S.Content>
